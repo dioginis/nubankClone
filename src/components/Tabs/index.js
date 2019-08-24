5a69ca2342d06b122249ec92f0d6a2381e1a3953
Dioginis <dioginiscarvalho@hotmail.com>
2019-08-24 12:33:53 -0300

    feat: finish app

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -2,9 +2,28 @@ import React from "react";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import { Container, TabsContainer, TabItem, TabText } from "./styles";
 
-export default function Tabs() {
+export default function Tabs({ translateY }) {
   return (
-    <Container>
+    <Container
+      style={{
+        transform: [
+          {
+            translateY: translateY.interpolate({
+              inputRange: [0, 380],
+              outputRange: [0, 30],
+              extrapolate: "clamp"
+            })
+          }
+        ]
+      }}
+      style={{
+        opacity: translateY.interpolate({
+          inputRange: [0, 380],
+          outputRange: [1, 0.3],
+          extrapolate: "clamp"
+        })
+      }}
+    >
       <TabsContainer>
         <TabItem>
           <Icon name="person-add" size={24} color="#FFF"></Icon>
